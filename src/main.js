9c97a58c845d5a9dfb8b7db2425c4645085e89c0
ian <gabionian7@gmail.com>
2021-12-06 09:00:01 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import vuetify from './plugins/vuetify'
+import axios from '@/plugins/axios'
+import store from './store'
+import router from './router'
+
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    vuetify,
+    axios,
+    store,
+    router,
+    render: h => h(App)
 }).$mount('#app')
